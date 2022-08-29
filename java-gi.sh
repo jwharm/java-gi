1db7c66808d3b9486c30ad59bbd9fc3798b2ee17
Jan-Willem Harmannij <jwharmannij@gmail.com>
2022-08-29 23:54:20 +0200

    Don't generate sources zip by default

--- java-gi.sh
+++ java-gi.sh
@@ -10,12 +10,12 @@ PKG_LIBS=`pkg-config --libs gdk-pixbuf-2.0 gtk4-unix-print gtk4`
 PKG_INCLUDES=`pkg-config --cflags-only-I gdk-pixbuf-2.0 gio-unix-2.0 gtk4-unix-print gtk4`
 
 # Generate java source files and generate java classes
-$JAVA_HOME/bin/jextract --source -d generated/src -t $PACKAGE_NAME $PKG_LIBS $PKG_INCLUDES gtk.h
 $JAVA_HOME/bin/jextract -d generated/bin -t $PACKAGE_NAME $PKG_LIBS $PKG_INCLUDES gtk.h
+# $JAVA_HOME/bin/jextract -d generated/src -t $PACKAGE_NAME $PKG_LIBS $PKG_INCLUDES gtk.h --source
 
 # Create jar file and sources jar file
 $JAVA_HOME/bin/jar cvf generated/lib/gtk-4.0.jar -C generated/bin .
-$JAVA_HOME/bin/jar cvf generated/lib/gtk-4.0-src.zip -C generated/src .
+# $JAVA_HOME/bin/jar cvf generated/lib/gtk-4.0-src.zip -C generated/src .
 
 # Cleanup java classes
 rm -rf generated/bin
