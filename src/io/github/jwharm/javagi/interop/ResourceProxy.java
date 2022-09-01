80b824762565a59bdba461eb0c8f28bd6dc4f470
Jan-Willem Harmannij <jwharmannij@gmail.com>
2022-09-01 22:41:57 +0200

    Add Cleaner

--- src/io/github/jwharm/javagi/interop/ResourceProxy.java
+++ src/io/github/jwharm/javagi/interop/ResourceProxy.java
@@ -2,19 +2,36 @@ package io.github.jwharm.javagi.interop;
 
 import jdk.incubator.foreign.MemoryAddress;
 
+import java.lang.ref.Cleaner;
+
 public class ResourceProxy implements NativeAddress {
 
-    private MemoryAddress __HANDLE__;
+    private final static Cleaner cleaner = Cleaner.create();
+    private final State state;
+    private final Cleaner.Cleanable cleanable;
+
+    private static class State implements Runnable {
+        MemoryAddress __HANDLE__;
+
+        State(MemoryAddress handle) {
+            __HANDLE__ = handle;
+        }
+
+        public void run() {
+            io.github.jwharm.javagi.interop.jextract.gtk_h.g_object_unref(__HANDLE__);
+        }
+    }
 
     public ResourceProxy(MemoryAddress handle) {
-        this.__HANDLE__ = handle;
+        state = new State(handle);
+        cleanable = cleaner.register(this, state);
     }
 
     public void setHANDLE(MemoryAddress handle) {
-        this.__HANDLE__ = handle;
+        state.__HANDLE__ = handle;
     }
 
     public MemoryAddress HANDLE() {
-        return __HANDLE__;
+        return state.__HANDLE__;
     }
 }
