44f377e01a4b937280994a3bb12e0250bf554a32
JFronny <git@akbvopenfl1.hopto.org>
2022-10-09 20:59:47 +0200

    module-info from java-gtk4

---
+++ gtk4/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.gtk {
+    exports io.github.jwharm.javagi;
+    exports org.cairographics;
+    exports org.gnome.adw;
+    exports org.gtk.gdk;
+    exports org.gtk.gdkpixbuf;
+    exports org.gtk.gio;
+    exports org.gtk.glib;
+    exports org.gtk.gobject;
+    exports org.gtk.graphene;
+    exports org.gtk.gsk;
+    exports org.gtk.gtk;
+    exports org.harfbuzz;
+    exports org.pango;
+}
