f8a14b92dd6ac66ff7b2f2d311a7380f1ade1cab
Jan-Willem Harmannij <jwharmannij@gmail.com>
2022-10-09 21:39:02 +0200

    Remove repository check; update .gitignore

--- generator/src/main/java/io/github/jwharm/javagi/generator/Conversions.java
+++ generator/src/main/java/io/github/jwharm/javagi/generator/Conversions.java
@@ -45,7 +45,6 @@ public class Conversions {
         int idx = typeName.indexOf('.');
         if (idx > 0) {
             String namespace = Conversions.namespaceToJavaPackage(typeName.substring(0, idx));
-            if (namespace == null) throw new RuntimeException("Could not get namespace for " + typeName);
             return namespace + "." + toCamelCase(typeName.substring(idx + 1), true);
         } else {
             return toCamelCase(typeName, true);
