a0ff5eebf93ebb5b95ccdb10eac7ed0fdf79c825
jwharm <112160955+jwharm@users.noreply.github.com>
2022-10-23 20:37:04 +0200

    Add some whitespace to example app

--- example/src/main/java/io/github/jwharm/javagi/example/HelloWorld.java
+++ example/src/main/java/io/github/jwharm/javagi/example/HelloWorld.java
@@ -9,11 +9,14 @@ public class HelloWorld {
         var window = new ApplicationWindow(Application.castFrom(g_application));
         window.setTitle("Window");
         window.setDefaultSize(300, 200);
+        
         var box = new Box(Orientation.VERTICAL, 0);
         box.setHalign(Align.CENTER);
         box.setValign(Align.CENTER);
+        
         var button = Button.newWithLabel("Hello world!");
         button.onClicked((btn) -> window.close());
+        
         box.append(button);
         window.setChild(box);
         window.show();
