3cf6a0f43cbbb449c8107c554d8add82b2c0e25d
JFronny <git@akbvopenfl1.hopto.org>
2022-12-16 19:01:00 +0100

    Use bindTo

--- generator/src/main/java/io/github/jwharm/javagi/model/Callback.java
+++ generator/src/main/java/io/github/jwharm/javagi/model/Callback.java
@@ -106,7 +106,7 @@ public class Callback extends RegisteredType implements CallableType {
         }
         writer.write(");\n");
         writer.write("            return Linker.nativeLinker().upcallStub(\n");
-        writer.write("                MethodHandles.insertArguments(MethodHandles.lookup().findVirtual(" + javaName + ".class, \"upcall\", Linker.upcallType(fd)), 0, this),\n");
+        writer.write("                MethodHandles.lookup().findVirtual(" + javaName + ".class, \"upcall\", Linker.upcallType(fd)).bindTo(this),\n");
         writer.write("                fd,\n");
         writer.write("                Interop.getScope()\n");
         writer.write("            ).address();\n");
