1f847c1e5092fee28427d40734ce06021d828782
JFronny <git@akbvopenfl1.hopto.org>
2022-12-28 17:12:14 +0100

    Support normal natives

--- glib/src/main/java/io/github/jwharm/javagi/LibLoad.java
+++ glib/src/main/java/io/github/jwharm/javagi/LibLoad.java
@@ -7,9 +7,16 @@ import java.util.stream.Stream;
 
 public class LibLoad {
     private static final boolean OS_WINDOWS = System.getProperty("os.name").toLowerCase().contains("win");
+    private static final boolean OS_MACOS = System.getProperty("os.name").toLowerCase().contains("mac");
+    private static final String LIB_SUFFIX;
+    static {
+        if (OS_WINDOWS) LIB_SUFFIX = ".dll";
+        else if (OS_MACOS) LIB_SUFFIX = ".dylib";
+        else LIB_SUFFIX = ".so";
+    }
 
     public static void loadLibrary(String name) {
-        RuntimeException fail = new RuntimeException("Could not load library");
+        InteropException fail = new InteropException("Could not load library");
         try {
             System.loadLibrary(name);
             return;
@@ -35,7 +42,7 @@ public class LibLoad {
                         System.load(path.toString());
                         return;
                     }
-                    if (OS_WINDOWS && fn.startsWith("lib" + name + "-") && fn.endsWith(".dll")) {
+                    if (OS_WINDOWS && (fn.startsWith("lib" + name + "-") || fn.startsWith(name + "-")) && fn.endsWith(LIB_SUFFIX)) {
                         System.load(path.toString());
                         return;
                     }
