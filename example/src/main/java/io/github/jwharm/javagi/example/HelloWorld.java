bde5e21110581ea15bd01ac07a034529dd85ee55
Jan-Willem Harmannij <jwharmannij@gmail.com>
2022-12-31 14:56:20 +0100

    Don't run listview example by default

--- example/src/main/java/io/github/jwharm/javagi/example/HelloWorld.java
+++ example/src/main/java/io/github/jwharm/javagi/example/HelloWorld.java
@@ -5,10 +5,13 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         // GTK4 example
-        new Gtk4ListViewExample(args);
+        new Gtk4Example(args);
+
+        // GTK4 ListView example, currently broken
+        // new Gtk4ListViewExample(args);
 
         // GStreamer example
         // To run example, provide a valid path to an Ogg Vorbis file:
-        // new GStreamerExample(new String[]{"Example.ogg"});
+        new GStreamerExample(new String[]{"Example.ogg"});
     }
 }
