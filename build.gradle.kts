cc44f374f6ae0d5ae345b6e70ddae4c6fb619d42
JFronny <git@akbvopenfl1.hopto.org>
2023-01-10 19:41:11 +0100

    Setup javadoc to pages publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,10 @@ tasks.register("publish") {
     dependsOn(gradle.includedBuild("generator").task(":publish"))
 }
 
+tasks.register("javadoc") {
+    dependsOn(gradle.includedBuild("generator").task(":javadoc"))
+}
+
 tasks.register("example") {
     dependsOn(project(":example").tasks["run"])
 }
