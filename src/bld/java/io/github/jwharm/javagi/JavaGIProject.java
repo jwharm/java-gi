ec998453717964a170f0cd7459d9445d577d3f12
Jan-Willem Harmannij <jwharmannij@gmail.com>
2023-05-01 13:50:13 +0200

    Set project-specific buildMainDirectories

--- src/bld/java/io/github/jwharm/javagi/JavaGIProject.java
+++ src/bld/java/io/github/jwharm/javagi/JavaGIProject.java
@@ -5,6 +5,7 @@ import rife.bld.BuildCommand;
 import rife.bld.Project;
 import rife.bld.operations.JavadocOptions;
 
+import java.io.File;
 import java.util.List;
 
 import static rife.bld.dependencies.Repository.MAVEN_CENTRAL;
@@ -16,12 +17,15 @@ public class JavaGIProject extends Project {
 
     public JavaGIProject(JavaGIBuild bld, String name) {
         javaRelease = 20;
-
         repositories = List.of(MAVEN_CENTRAL);
+        buildMainDirectory = new File(bld.buildMainDirectory(), name);
+        buildJavadocDirectory = new File(bld.buildJavadocDirectory(), name);
+        javaGIOperation_ = new JavaGIOperation();
+
         scope(compile)
             .include(dependency("org.jetbrains", "annotations", version(24,0,1)));
 
-        javaGIOperation_ = new JavaGIOperation()
+        javaGIOperation()
             .sourceDirectory(bld.girDirectory())
             .outputDirectory(bld.buildDirectory().toPath().resolve("generated").resolve(name))
             .platform(Platform.LINUX);
@@ -39,9 +43,6 @@ public class JavaGIProject extends Project {
                 .enablePreview()
                 .docLint(JavadocOptions.DocLinkOption.NO_MISSING) // ignore lint errors
                 .quiet();
-
-        // bld ignores the configured javadoc output directory, but not this one:
-        buildJavadocDirectory = bld.buildDirectory().toPath().resolve("javadoc").resolve(name).toFile();
 
         jarSourcesOperation()
             .sourceDirectories(javaGIOperation().outputDirectory().toFile());
