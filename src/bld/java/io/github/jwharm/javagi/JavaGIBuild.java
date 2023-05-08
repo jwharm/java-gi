136ae16e1de9eea7e28014abf7dcd3f76d86bf2a
Jan-Willem Harmannij <jwharmannij@gmail.com>
2023-05-09 00:33:16 +0200

    Fix issue with sources-jars in module-path

--- src/bld/java/io/github/jwharm/javagi/JavaGIBuild.java
+++ src/bld/java/io/github/jwharm/javagi/JavaGIBuild.java
@@ -1,10 +1,7 @@
 package io.github.jwharm.javagi;
 
 import rife.bld.BuildCommand;
-import rife.bld.publish.*;
-
 import java.nio.file.Path;
-import java.util.stream.Stream;
 
 /**
  * Base class that is run by the `bld` command.
@@ -34,31 +31,6 @@ public class JavaGIBuild extends ModularProject {
             .uri(URI)
             .directory(buildDirectory().toPath().resolve("gir-files"));
         girDirectory(gitPullOperation_.directory());
-
-        publishOperation()
-            .repository(repository("https://maven.pkg.github.com/jwharm/javagi"))
-            .info(new PublishInfo()
-                .groupId("io.github.jwharm.javagi")
-                .artifactId("javagi")
-                .description("Java bindings generated from GObject-Introspection")
-                .url("https://jwharm.github.io/java-gi/")
-                .developer(new PublishDeveloper()
-                    .id("jwharm")
-                    .name("Jan-Willem Harmannij")
-                    .url("https://github.com/jwharm"))
-                .license(new PublishLicense()
-                    .name("GNU Lesser General Public License, version 3")
-                    .url("https://www.gnu.org/licenses/lgpl-3.0.txt"))
-                .scm(new PublishScm()
-                    .connection("scm:git:https://github.com/jwharm/javagi.git")
-                    .developerConnection("scm:git:git@github.com:jwharm/javagi.git")
-                    .url("https://jwharm.github.io/java-gi/")))
-            .artifacts(
-                Stream.of(
-                    modules().stream().map(m -> new PublishArtifact(m.jarOperation().destinationFile(), m.name(), "jar")),
-                    modules().stream().map(m -> new PublishArtifact(m.jarJavadocOperation().destinationFile(), m.name(), "jar")),
-                    modules().stream().map(m -> new PublishArtifact(m.jarSourcesOperation().destinationFile(), m.name(), "jar"))
-                ).flatMap(i -> i).toList());
     }
 
     @BuildCommand(value="download-gir-files", summary="Pull gir files from remote repository")
--- src/bld/java/io/github/jwharm/javagi/JavaGIProject.java
+++ src/bld/java/io/github/jwharm/javagi/JavaGIProject.java
@@ -3,14 +3,23 @@ package io.github.jwharm.javagi;
 import rife.bld.BuildCommand;
 import rife.bld.Project;
 import rife.bld.operations.JavadocOptions;
+import rife.bld.publish.PublishDeveloper;
+import rife.bld.publish.PublishInfo;
+import rife.bld.publish.PublishLicense;
+import rife.tools.FileUtils;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import static rife.bld.dependencies.Repository.MAVEN_CENTRAL;
+import static rife.bld.dependencies.Repository.MAVEN_LOCAL;
 import static rife.bld.dependencies.Scope.compile;
 
 public class JavaGIProject extends Project {
+
+    public static final Pattern SOURCES_JAR_FILE_PATTERN = Pattern.compile("^.*-sources\\.jar$");
 
     private final GenerateSourcesOperation generateSourcesOperation_;
 
@@ -32,12 +41,12 @@ public class JavaGIProject extends Project {
         compileOperation()
             .mainSourceDirectories(generateSourcesOperation().outputDirectory().toFile())
             .compileOptions()
-                .modulePath(libCompileDirectory(), buildDistDirectory())
+                .modulePath(getModulePath())
                 .enablePreview();
 
         javadocOperation()
             .sourceDirectories(generateSourcesOperation().outputDirectory().toFile())
-            .javadocOptions(List.of("--module-path", libCompileDirectory() + ":" + buildDistDirectory()))
+            .javadocOptions(List.of("--module-path", getModulePathString()))
             .javadocOptions()
                 .enablePreview()
                 .docLint(JavadocOptions.DocLinkOption.NO_MISSING)
@@ -45,10 +54,40 @@ public class JavaGIProject extends Project {
 
         jarSourcesOperation()
             .sourceDirectories(generateSourcesOperation().outputDirectory().toFile());
+
+        publishOperation()
+            .repository(MAVEN_LOCAL)
+            .info(new PublishInfo()
+                .groupId("io.github.jwharm.javagi")
+                .artifactId(name)
+                .description("Java bindings for " + name + ", generated from GObject-Introspection")
+                .url("https://jwharm.github.io/java-gi/")
+                .developer(new PublishDeveloper()
+                    .id("jwharm")
+                    .name("Jan-Willem Harmannij")
+                    .url("https://github.com/jwharm"))
+                .license(new PublishLicense()
+                    .name("GNU Lesser General Public License, version 3")
+                    .url("https://www.gnu.org/licenses/lgpl-3.0.txt")));
     }
 
     public GenerateSourcesOperation generateSourcesOperation() {
         return generateSourcesOperation_;
+    }
+
+    /* Create the module-path: The lib/compile directory is in the module path, and the
+     * generated jar files, excluding sources-jar files.
+     */
+    private List<File> getModulePath() {
+        List<File> modules = new ArrayList<>();
+        modules.add(libCompileDirectory());
+        modules.addAll(FileUtils.getFileList(buildDistDirectory(), FileUtils.JAR_FILE_PATTERN, SOURCES_JAR_FILE_PATTERN)
+            .stream().map(file -> new File(buildDistDirectory(), file)).toList());
+        return modules;
+    }
+
+    private String getModulePathString() {
+        return FileUtils.joinPaths(getModulePath().stream().map(File::getAbsolutePath).toList());
     }
 
     @BuildCommand(value="generate-sources", summary="Generates Java sources from gir files")
@@ -59,6 +98,16 @@ public class JavaGIProject extends Project {
     @Override
     public void compile() throws Exception {
         generateSources();
+        compileOperation().compileOptions().modulePath(getModulePath());
         super.compile();
+    }
+
+    /**
+     * Overrides {@link Project#javadoc()} to avoid recompiling the project
+     */
+    @Override
+    public void javadoc() throws Exception {
+        javadocOperation().javadocOptions(List.of("--module-path", getModulePathString()));
+        javadocOperation().executeOnce(() -> javadocOperation().fromProject(this));
     }
 }
