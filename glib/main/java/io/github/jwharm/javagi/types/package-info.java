ffc8af22196bbb2973c840e34e893ce6d70487cd
Jan-Willem Harmannij <jwharmannij@gmail.com>
2023-06-03 13:50:18 +0200

    Update javadoc for javagi.types package

--- glib/main/java/io/github/jwharm/javagi/types/package-info.java
+++ glib/main/java/io/github/jwharm/javagi/types/package-info.java
@@ -1,5 +1,6 @@
 /**
  * This package contains functionality to register a Java class as
- * a new GType in the GObject type system.
+ * a new GType in the GObject type system, and various constants and
+ * utility functions for working with GTypes and GVariants.
  */
 package io.github.jwharm.javagi.types;
