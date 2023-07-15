57cd1ade665633f6e5b29172c8a5e60e784bf052
Jan-Willem Harmannij <jwharmannij@gmail.com>
2023-07-15 11:20:34 +0200

    Add missing javadoc

--- glib/main/java/io/github/jwharm/javagi/base/ProxyInstance.java
+++ glib/main/java/io/github/jwharm/javagi/base/ProxyInstance.java
@@ -22,10 +22,21 @@ public class ProxyInstance implements Proxy {
         MemoryCleaner.register(this);
     }
 
+    /**
+     * Set the flag that determines if for virtual method calls, {@code g_type_class_peek_parent()}
+     * is used to obtain the function pointer of the parent type instead of the instance class.
+     * @param callParent true if you want to call the parent vfunc instead of an overrided vfunc
+     */
     protected void callParent(boolean callParent) {
         this.callParent = callParent;
     }
 
+    /**
+     * Returns the flag that determines if for virtual method calls, {@code g_type_class_peek_parent()}
+     * is used to obtain the function pointer of the parent type instead of the instance class.
+     * @return true when parent vfunc is called instead of an overrided vfunc, or false when the
+     *         overrided vfunc of the instance is called.
+     */
     public boolean callParent() {
         return this.callParent;
     }
