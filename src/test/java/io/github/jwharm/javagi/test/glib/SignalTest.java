71704700ac2c9481f09f8c9b8a5bb20b5c0b98bd
Jan-Willem Harmannij <jwharmannij@gmail.com>
2023-07-28 22:40:47 +0200

    Add tests for signal handling

---
+++ src/test/java/io/github/jwharm/javagi/test/glib/SignalTest.java
@@ -0,0 +1,58 @@
+package io.github.jwharm.javagi.test.glib;
+
+import io.github.jwharm.javagi.base.Signal;
+import org.gnome.gio.Application;
+import org.gnome.gio.ApplicationFlags;
+import org.gnome.gobject.GObject;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Test connecting a signal, and blocking/unblocking it
+ */
+public class SignalTest {
+
+    @Test
+    public void connectSignal() {
+        var success = new AtomicBoolean(false);
+        Application app = new Application("test.id1", ApplicationFlags.DEFAULT_FLAGS);
+        Signal<GObject.Notify> signal = app.onNotify("application-id", paramSpec -> {
+            success.set(true);
+        });
+        assertTrue(signal.isConnected());
+        app.setApplicationId("test.id2");
+        assertTrue(success.get());
+    }
+
+    @Test
+    public void disconnectSignal() {
+        var success = new AtomicBoolean(true);
+        Application app = new Application("test.id1", ApplicationFlags.DEFAULT_FLAGS);
+        Signal<GObject.Notify> signal = app.onNotify("application-id", paramSpec -> {
+            success.set(false);
+        });
+        signal.disconnect();
+        assertFalse(signal.isConnected());
+        app.setApplicationId("test.id2");
+        assertTrue(success.get());
+    }
+
+    @Test
+    public void blockUnblockSignal() {
+        var success = new AtomicBoolean(true);
+        Application app = new Application("test.id1", ApplicationFlags.DEFAULT_FLAGS);
+        Signal<GObject.Notify> signal = app.onNotify("application-id", paramSpec -> {
+            success.set(false);
+        });
+        signal.block();
+        app.setApplicationId("test.id2");
+        assertTrue(success.get());
+        signal.unblock();
+        app.setApplicationId("test.id3");
+        assertFalse(success.get());
+    }
+}
