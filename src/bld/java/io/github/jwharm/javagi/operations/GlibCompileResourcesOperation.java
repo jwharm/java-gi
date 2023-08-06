00289f4064db97efa30a2c048af6e066415abfc5
Jan-Willem Harmannij <jwharmannij@gmail.com>
2023-08-06 20:24:40 +0200

    Add license banner

--- src/bld/java/io/github/jwharm/javagi/operations/GlibCompileResourcesOperation.java
+++ src/bld/java/io/github/jwharm/javagi/operations/GlibCompileResourcesOperation.java
@@ -1,3 +1,22 @@
+/* Java-GI - Java language bindings for GObject-Introspection-based libraries
+ * Copyright (C) 2022-2023 Jan-Willem Harmannij
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 package io.github.jwharm.javagi.operations;
 
 import rife.bld.operations.AbstractOperation;
@@ -5,11 +24,9 @@ import rife.bld.operations.exceptions.ExitStatusException;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.Future;
 
 public class GlibCompileResourcesOperation extends AbstractOperation<GlibCompileResourcesOperation> {
 
