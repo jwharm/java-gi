475dfc28e6f6c95878ab2fdb4f2aa853168899a4
Jan-Willem Harmannij <jwharmannij@gmail.com>
2024-07-14 16:45:57 +0200

    Add missing parameter javadoc

--- modules/glib/src/main/java/io/github/jwharm/javagi/interop/Interop.java
+++ modules/glib/src/main/java/io/github/jwharm/javagi/interop/Interop.java
@@ -1039,6 +1039,7 @@ public class Interop {
      *
      * @param  <T>      an enum implementing the Java-GI Enumeration interface
      * @param  cls      the class of the enum
+     * @param  make     function that will construct an enum from one flag value
      * @param  bitfield the integer containing the bitfield
      * @return an EnumSet containing the enum values as set in the bitfield
      */
