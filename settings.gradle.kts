3fb5fc47ed03735e9c14897e68a3b0560a4610f1
Jan-Willem Harmannij <jwharmannij@gmail.com>
2024-10-26 17:18:38 +0200

    Fix rootproject name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-rootProject.name = "java-gi-cmd"
+rootProject.name = "java-gi"
 
 includeBuild("generator")
 include ("adw")
