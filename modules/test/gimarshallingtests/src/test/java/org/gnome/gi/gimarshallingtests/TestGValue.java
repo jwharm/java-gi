2199a39f0ac475a7b455cea9a416f289be98da0c
Jan-Willem Harmannij <jwharmannij@gmail.com>
2025-07-20 14:32:56 +0200

    Add regression tests

--- modules/test/gimarshallingtests/src/test/java/org/gnome/gi/gimarshallingtests/TestGValue.java
+++ modules/test/gimarshallingtests/src/test/java/org/gnome/gi/gimarshallingtests/TestGValue.java
@@ -21,6 +21,7 @@ package org.gnome.gi.gimarshallingtests;
 
 import org.gnome.gobject.Value;
 import org.javagi.base.Out;
+import org.javagi.gobject.types.Types;
 import org.junit.jupiter.api.Test;
 
 import static org.gnome.gi.gimarshallingtests.GIMarshallingTests.*;
@@ -46,5 +47,171 @@ public class TestGValue {
         Value v = gvalueNoncanonicalNanDouble();
         assertNotNull(v);
         assertEquals(Double.NaN, v.getDouble());
+    }
+
+    @Test
+    void in() {
+        Value v = new Value();
+        v.init(Types.INT);
+        v.setInt(42);
+        gvalueIn(v);
+    }
+
+    @Test
+    void int64In() {
+        Value v = new Value();
+        v.init(Types.INT64);
+        v.setInt64(Long.MAX_VALUE);
+        gvalueInt64In(v);
+    }
+
+    @Test
+    void inWithType() {
+        Value i = new Value();
+        i.init(Types.INT);
+        i.setInt(42);
+        gvalueInWithType(i, Types.INT);
+
+        Value d = new Value();
+        d.init(Types.DOUBLE);
+        d.setDouble(42.5d);
+        gvalueInWithType(d, Types.DOUBLE);
+    }
+
+    @Test
+    void inWithModification() {
+        Value v = new Value();
+        v.init(Types.INT);
+        v.setInt(42);
+        gvalueInWithModification(v);
+        assertEquals(24, v.getInt());
+    }
+
+    @Test
+    void inEnum() {
+        Value v = new Value();
+        v.init(Types.ENUM);
+        v.setEnum(Enum.VALUE3.getValue());
+        gvalueInEnum(v);
+    }
+
+    @Test
+    void inFlags() {
+        Value v = new Value();
+        v.init(Types.FLAGS);
+        v.setFlags(Flags.VALUE3.getValue());
+        gvalueInFlags(v);
+    }
+
+    @Test
+    void out() {
+        var out = new Out<Value>();
+        gvalueOut(out);
+        assertEquals(42, out.get().getInt());
+    }
+
+    @Test
+    void outUninitialized() {
+        var out = new Out<Value>();
+        assertFalse(gvalueOutUninitialized(out));
+        assertNull(out.get());
+    }
+
+    @Test
+    void int64Out() {
+        var out = new Out<Value>();
+        gvalueInt64Out(out);
+        assertEquals(Long.MAX_VALUE, out.get().getInt64());
+    }
+
+    @Test
+    void outCallerAllocates() {
+        Value v = new Value();
+        gvalueOutCallerAllocates(v);
+        assertEquals(42, v.getInt());
+    }
+
+    @Test
+    void inOut() {
+        Value v = new Value();
+        v.init(Types.INT);
+        v.setInt(42);
+        var out = new Out<>(v);
+        gvalueInout(out);
+        assertEquals("42", v.getString());
+    }
+
+    @Test
+    void flatArray() {
+        Value i = new Value();
+        i.init(Types.INT);
+        i.setInt(42);
+
+        Value s = new Value();
+        s.init(Types.STRING);
+        s.setString("42");
+
+        Value b = new Value();
+        b.init(Types.BOOLEAN);
+        b.setBoolean(true);
+
+        Value[] values = new Value[] {i, s, b};
+        gvalueFlatArray(values);
+    }
+
+    @Test
+    void returnFlatArray() {
+        Value[] values = returnGvalueFlatArray();
+        assertNotNull(values);
+        assertEquals(3, values.length);
+        assertEquals(42, values[0].getInt());
+        assertEquals("42", values[1].getString());
+        assertTrue(values[2].getBoolean());
+    }
+
+    @Test
+    void returnZeroTerminatedArray() {
+        Value[] values = returnGvalueZeroTerminatedArray();
+        assertNotNull(values);
+        assertEquals(3, values.length);
+        assertEquals(42, values[0].getInt());
+        assertEquals("42", values[1].getString());
+        assertTrue(values[2].getBoolean());
+    }
+
+    @Test
+    void roundTrip() {
+        Value a = new Value();
+        a.init(Types.INT);
+        a.setInt(42);
+        Value b = gvalueRoundTrip(a);
+        assertNotNull(b);
+        assertEquals(a.readGType(), b.readGType());
+        assertEquals(a.getInt(), b.getInt());
+    }
+
+    @Test
+    void copy() {
+        Value a = new Value();
+        a.init(Types.INT);
+        a.setInt(42);
+        Value b = gvalueCopy(a);
+        assertNotNull(b);
+        assertNotEquals(a, b);
+        assertEquals(a.readGType(), b.readGType());
+        assertEquals(a.getInt(), b.getInt());
+    }
+
+    @Test
+    void float_() {
+        Value f = new Value();
+        f.init(Types.FLOAT);
+        f.setFloat(3.14f);
+
+        Value d = new Value();
+        d.init(Types.DOUBLE);
+        d.setDouble(3.14d);
+
+        gvalueFloat(f, d);
     }
 }
