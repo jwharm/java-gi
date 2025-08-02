34ca7610d84c299f624985e55771033cfce9436b
Jan-Willem Harmannij <jwharmannij@gmail.com>
2025-08-02 12:06:44 +0200

    Rewrite metadata parser

---
+++ generator/src/main/java/org/javagi/metadata/Token.java
@@ -0,0 +1,30 @@
+/* Java-GI - Java language bindings for GObject-Introspection-based libraries
+ * Copyright (C) 2022-2025 the Java-GI developers
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.javagi.metadata;
+
+/**
+ * Represents a single token that is read by the metadata scanner.
+ *
+ * @param type     the token type
+ * @param text     the parsed lexeme (character string)
+ * @param position the position of the token in the metadata source file
+ */
+record Token(TokenType type, String text, int position) {
+}
