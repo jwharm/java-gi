32a0d46bdee8487362da0dff146d36a378446878
Jan-Willem Harmannij <jwharmannij@gmail.com>
2025-09-20 16:21:01 +0200

    Add regression tests

---
+++ modules/gobject-introspection-tests/src/test/java/org/javagi/regress/TestObjectMethods.java
@@ -0,0 +1,44 @@
+/* Java-GI - Java language bindings for GObject-Introspection-based libraries
+ * Copyright (C) 2025 Jan-Willem Harmannij
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.javagi.regress;
+
+import org.gnome.gi.regress.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestObjectMethods {
+    @Test
+    void instanceMethod() {
+        var o = new TestObj();
+        assertEquals(-1, o.instanceMethod());
+    }
+
+    @Test
+    void instanceMethodFull() {
+        var o = TestObj.builder().build();
+        o.instanceMethodFull();
+    }
+
+    @Test
+    void staticMethod() {
+        assertEquals(5, TestObj.staticMethod(5));
+    }
+}
